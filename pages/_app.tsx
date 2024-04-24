813e64a885054606081ee70fc1991e43eccb301e
avisehgal <vshgl2@gmail.com>
2024-04-24 11:20:18 +0530

    Better Dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,63 +2,51 @@ import '../styles/globals.css';
 import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
+import { IoMoon } from "react-icons/io5";
+import { IoSunny } from "react-icons/io5";
+import { IconContext } from "react-icons";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [darkMode, setDarkMode] = useState(false);
-
-  // When the component mounts and updates, check local storage
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    setDarkMode(isDarkMode);
-  }, []);
-
-  // Use useEffect to update the body's class and local storage when darkMode changes
-  useEffect(() => {
-    const className = 'dark';
-    if (darkMode) {
-      document.documentElement.classList.add(className);
-      localStorage.setItem('darkMode', 'true');
-    } else {
-      document.documentElement.classList.remove(className);
-      localStorage.setItem('darkMode', 'false');
-    }
-  }, [darkMode]);
-
-  if (darkMode) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
-
-  return (
-    <div className="container mx-auto px-4 bg-white dark:bg-black">
-      <header className="flex justify-between items-center py-6 font-quicksand">
-        {/* Left side of the nav */}
-        <nav className="flex-grow">
-        
-        </nav>
-        {/* Right side of the nav - Dark mode toggle */}
+    const [darkMode, setDarkMode] = useState(false);
+  
+    useEffect(() => {
+      const isDarkMode = localStorage.getItem('darkMode') === 'true';
+      setDarkMode(isDarkMode);
+    }, []);
+  
+    useEffect(() => {
+      localStorage.setItem('darkMode', darkMode.toString());
+      if (darkMode) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    }, [darkMode]);
+  
+    return (
+      <div className="container mx-auto px-4 bg-white dark:bg-black transition-colors duration-500">
+        <header className="flex justify-between items-center py-6 font-quicksand">
         <div>
             <Link href="/values" passHref><span className="text-lg text-gray-700 hover:text-gray-900 mx-2 cursor-pointer">About</span></Link>
             <Link href="/blog" passHref><span className="text-lg text-gray-700 hover:text-gray-900 mx-2 cursor-pointer">Blog</span></Link>
             <Link href="/projects" passHref><span className="text-lg text-gray-700 hover:text-gray-900 mx-2 cursor-pointer">Projects</span></Link>
             <Link href="/photography" passHref><span className="text-lg text-gray-700 hover:text-gray-900 mx-2 cursor-pointer">Photography</span></Link>
-          </div>
-        <button
-          onClick={() => setDarkMode(!darkMode)}
-          className="text-gray-800 dark:text-gray-200 px-3 py-1 rounded"
-        >
-          {darkMode ? 'Light Mode' : 'Dark Mode'}
-        </button>
-      </header>
-      <main className="container mx-auto px-4 bg-white dark:bg-black">
-        <Component {...pageProps} />
-      </main>
-      <footer className="text-center py-6 bg-white dark:bg-black">
-        <p className="text-gray-600 dark:text-gray-400">© {new Date().getFullYear()} Avi Sehgal</p>
-      </footer>
-    </div>
-  );
-}
+        </div>
+          <button onClick={() => setDarkMode(!darkMode)} className="text-gray-800 dark:text-gray-200 px-3 py-1 rounded">
+            <IconContext.Provider value={{ size: "1.5em" }}>
+              {darkMode ? <IoSunny /> : <IoMoon />}
+            </IconContext.Provider>
+          </button>
+        </header>
+        <main className="bg-white dark:bg-black transition-colors duration-500">
+          <Component {...pageProps} />
+        </main>
+        <footer className="text-center py-6 bg-white dark:bg-black transition-colors duration-500">
+          <p className="text-gray-600 dark:text-gray-400">© {new Date().getFullYear()} Avi Sehgal</p>
+        </footer>
+      </div>
+    );
+  }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,22 @@ import type { NextPage } from 'next';
 import Image from 'next/image'; 
 
 const Home: NextPage = () => {
-  return (
-    <div className="mx-auto px-4 relative bg-white dark:bg-black"> {/* Added relative positioning */}
-      <header className="relative text-center py-6">
-        {/* Cover Image as background */}
-        <div className="cover relative h-[300px] "> {/* Set a fixed height for the cover */}
+    return (
+      <div className="mx-auto px-4 relative bg-white dark:bg-black transition-colors duration-500">
+        <header className="relative text-center py-6">
+          <div className="cover relative h-[300px] bg-white dark:bg-black transition-colors duration-500">
             <Image
-                src="https://i.ibb.co/bHC4LT0/cover.jpg"
-                alt="Cover Image"
-                layout="fill"
-                className="image"
-                quality={100}
+              src="https://i.ibb.co/bHC4LT0/cover.jpg"
+              alt="Cover Image"
+              layout="fill"
+              className="image"
+              quality={100}
             />
-            <div className="absolute inset-0 flex items-center justify-center"> {/* Absolute positioning to center text */}
-            <h1 className="text-7xl text-white font-hindmadurai" style={{ WebkitTextStroke: '1px black' }}>Avi Sehgal</h1>
+            <div className="absolute inset-0 flex items-center justify-center">
+              <h1 className="text-7xl text-white font-hind-madurai dark:text-stroke">Avi Sehgal</h1>
             </div>
-        </div>
-      </header>
+          </div>
+        </header>
       <main>
         <section className="mt-8 pt-48"> {/* Padding top to push content below the header image */}
           <h2 className="text-2xl font-semibold">Welcome</h2>
